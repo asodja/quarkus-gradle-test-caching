4e12a6ebf256f471758f293c9158b6e6bb21bffe
Anze Sodja <anze.sodja@outfit7.com>
2020-08-16 20:40:04 +0200

    Initial commit

---
+++ src/test/java/org/my/group/MyResourceTest.java
@@ -0,0 +1,39 @@
+package org.my.group;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+@QuarkusTest
+public class MyResourceTest {
+
+    @Test
+    public void testHelloEndpoint() {
+        given()
+                .when().get("/hello")
+                .then()
+                .statusCode(200)
+                .body(is("hello"));
+    }
+
+    @Test
+    public void testHelloEndpoint2() {
+        given()
+                .when().get("/hello")
+                .then()
+                .statusCode(200)
+                .body(is("hello"));
+    }
+
+    @Test
+    public void testHelloEndpoint3() {
+        given()
+                .when().get("/hello")
+                .then()
+                .statusCode(200)
+                .body(is("hello"));
+    }
+
+}
